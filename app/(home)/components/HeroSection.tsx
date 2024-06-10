cfe212474138a9c57189302fefa378b4f20d649d
AbhishekSinghDev <singhabhishek7689@gmail.com>
2024-06-10 21:52:05 +0530

    update: resume link

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
         <div className="absolute bottom-5 sm:bottom-14 left-0 sm:-left-10">
           <MovingBorderBtn borderRadius="0.5rem" className="p-3 font-semibold">
             <Link
-              href="https://drive.google.com/file/d/19SSd5jyVWQpBeuZAklbLRyj8eCHtmLyC/view"
+              href="https://drive.google.com/file/d/1hbrSLgSR-9GQSzwZF9DTxB6ez6kqPltZ/view"
               target="_blank"
             >
               <p>📢 Available for Work</p>
